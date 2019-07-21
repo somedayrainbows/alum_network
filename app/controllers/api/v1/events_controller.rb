b6246a986d7f3fa79c494a1441bf44c226fefcd3
Erin Bassity <erin.bassity@cognizant.com>
2019-07-21 15:16:38 -0600

    Add events controller
    * Add pry
    * Update spec

---
+++ app/controllers/api/v1/events_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::EventsController < ApplicationController
+
+  def index
+    render json: Event.all
+  end
+end
--- spec/requests/api/v1/events_request_spec.rb
+++ spec/requests/api/v1/events_request_spec.rb
@@ -5,7 +5,10 @@ describe 'Events API' do
     create_list(:event, 3)
 
     get '/api/v1/events'
+    binding.pry
 
     expect(response).to be_successful
+
+    events = JSON.parse(response.body)
   end
 end
