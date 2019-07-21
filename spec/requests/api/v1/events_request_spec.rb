0ab684d57fac7517edc85d3f56882bbb5da54c10
Erin Bassity <erin.bassity@cognizant.com>
2019-07-21 16:50:30 -0600

    Add spec and endpoint for single event retrieval

--- spec/requests/api/v1/events_request_spec.rb
+++ spec/requests/api/v1/events_request_spec.rb
@@ -12,4 +12,18 @@ describe 'Events API' do
 
     expect(events.count).to eq(3)
   end
+
+  it 'can get a single event by its id' do
+    id = create(:event).id
+
+    get "/api/v1/events/#{id}"
+
+    expect(response).to be_successful
+
+    event = JSON.parse(response.body)
+
+    expect(event["id"]).to eq(id)
+  end
+
+
 end
