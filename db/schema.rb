caf171557f80340384d2d594341c432ea31c2ed4
Erin Bassity <erin.bassity@cognizant.com>
2019-07-23 17:28:30 -0600

    Update migrations and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_154426) do
+ActiveRecord::Schema.define(version: 2019_07_23_232453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    admin: false,
+    role: "alumni",
+    city: "Denver",
+    state: "CO",
+    country: "U.S.",
+    cohort: "#{rand(12..19)}#{rand(10..12)}"
+  )
+  puts "10 user seeds created."
+end
+
+10.times do
+  Event.create!(
+    title: Faker::Movies::HarryPotter.house + " meeting",
+    date: Faker::Date.between(Date.today, 2.years.from_now),
+    event_notes: Faker::Hipster.paragraphs(1)
+  )
+  puts "10 event seeds created."
+end
+
+50.times do
+  UserEvent.create!(
+    user_id: rand(1..10),
+    event_id: rand(1..10)
+  )
+  puts "50 user_event seeds created."
+end
